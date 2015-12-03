85620963d17b3236e01286703104d50ab8a928be
Luke Poskitt <luke.poskitt@gmail.com>
2015-12-04 08:55:15 +1100

    Added pids/destination class.

---
+++ lib/pids/destination.rb
@@ -0,0 +1,14 @@
+module Pids
+
+  class Destination
+    attr_reader :route_no, :up_stop, :deatination
+    
+    def initialize( route_no, up_stop, destination )
+      @route_no = route_no
+      @up_stop = up_stop
+      @destination = destination
+    end
+
+  end
+
+end
