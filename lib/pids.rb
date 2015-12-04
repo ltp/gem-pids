a2122719abc22deefa954815c658133b0b78a28d
Luke Poskitt <luke.poskitt@gmail.com>
2015-12-04 14:22:38 +1100

    New methods and also an attempt at metaprogramming for method auto-generation.

--- lib/pids.rb
+++ lib/pids.rb
@@ -1,12 +1,17 @@
 require "pids/version"
-#require "pids/route"
+require "pids/route"
 require 'savon'
-#require 'pids/route'
+require 'pids/destination'
 
 module Pids
 
   class Client
-    attr_accessor :client_guid, :client_type, :client_version, :client_webservice_version, :routes
+    attr_accessor :client_guid,
+                  :client_type,
+                  :client_version,
+                  :client_webservice_version,
+                  :route_numbers,
+                  :routes
 
     def initialize( params = {})
       @client_guid = params[:client_guid] || get_client_guid
@@ -14,6 +19,7 @@ module Pids
       @client_version = params[:client_version] || '0.0.1'
       @client_webservice_version = params[:client_webservice_version] || '6.4.0.0'
       @routes = []
+      @route_numbers = []
       @destinations = []
 
       @client = Savon.client(
@@ -44,35 +50,75 @@ module Pids
           }
         },
       )
-      client.call(:get_new_client_guid).body[:get_new_client_guid_response][:get_new_client_guid_result]
+      client.call(:get_new_client_guid).
+        body[:get_new_client_guid_response][:get_new_client_guid_result]
     end
 
     def get_main_routes
       response = @client.call(:get_main_routes)
-      routes = response.body[:get_main_routes_response][:get_main_routes_result][:diffgram][:document_element][:list_of_non_sub_routes]
+      routes = response.body[:get_main_routes_response]\
+                            [:get_main_routes_result]\
+                            [:diffgram]\
+                            [:document_element]\
+                            [:list_of_non_sub_routes]
 
       routes.each do |route|
-        @routes.push(route[:route_no])
+        @route_numbers.push(route[:route_no])
       end
 
-      #@client.call(:get_main_routes).body[:get_main_routes_response][:get_main_routes_result][:diffgram][:document_element][:list_of_non_sub_routes].each do |route|
-      #  @routes.push(route[:route_no])
-      #end
+      @route_numbers
+    end
+
+    def get_destinations_for_all_routes
+      @client.call(:get_destinations_for_all_routes)
+        .body[:get_destinations_for_all_routes_response]\
+             [:get_destinations_for_all_routes_result]\
+             [:diffgram]\
+             [:document_element]\
+             [:list_of_destinations_for_all_routes]
+        .each do |dest|
+          @destinations.push(Pids::Destination.new(
+                                               dest[:route_no],
+                                               dest[:up_stop],
+                                               dest[:destination] 
+                                             ))
+        end
+
+      @destinations
+    end
+
+    #methods = 
+
+    ['get_route_summaries'].each do |m|
+      define_method m do
+      @client.call(m.to_sym)
+      .body["#{m}_response".to_sym]["#{m}_result".to_sym][:diffgram][:document_element][:route_summaries]
+      end
+    end
+
+    def __get_route_summaries
+      @client.call(:get_route_summaries)
+        .body[:get_route_summaries_response]\
+             [:get_route_summaries_result]\
+             [:diffgram]\
+             [:document_element]\
+             [:route_summaries]
+        .each do |route|
+          @routes.push(Pids::Route.new(route[:route_no], route[:headboard_route_no]))
+        end
 
       @routes
     end
 
-    def get_destinations_for_all_routes
-      response = @client.call(:get_destinations_for_all_routes)
-      dests = response.body[:get_destinations_for_all_routes_response][:get_destinations_for_all_routes_result][:diffgram][:document_element][:list_of_destinations_for_all_routes]
-
-      dests.each do |dest|
-        @destinations.push({"route_no" => dest[:route_no], "up_stop" => dest[:up_stop], "destination" => dest[:destination]})
-      end
-
-      @destinations
+    def get_stop_information(stop_id)
+      response = @client.call(:get_stop_information)
     end
-      
+
+    def get_list_of_stops_by_route_no_and_direction
+      response = @client.call(:get_list_of_stops_by_route_no_and_direction).body
+      response
+    end
+
   end
       
 end
