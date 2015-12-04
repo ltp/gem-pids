ebd8cb5c17e25383c53169aae7eca0792ca8d284
Luke Poskitt <luke.poskitt@gmail.com>
2015-12-04 14:21:23 +1100

    American "s" instead of Australian "s" in initiali"z"e.

--- lib/pids/route.rb
+++ lib/pids/route.rb
@@ -1,9 +1,13 @@
 module Pids
 
   class Route
+    attr_reader :route_no, 
+                :headboard_route_no,
+                :internal_route_no
     
-    def initialise( route_no )
+    def initialize( route_no, headboard_route_no )
       @route_no = route_no
+      @headboard_route_no = headboard_route_no
     end
 
   end
