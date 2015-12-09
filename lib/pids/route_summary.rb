513e6fd2fd412131fdd9f485a71f1ec8d977ea06
Luke Poskitt <luke.poskitt@gmail.com>
2015-12-09 15:03:44 +1100

    Added additional accessor methods.

--- lib/pids/route_summary.rb
+++ lib/pids/route_summary.rb
@@ -3,7 +3,7 @@ module Pids
   class RouteSummary
     attr_reader :route_no, 
                 :headboard_route_no,
-                :internal_route_no
+                :internal_route_no,
                 :is_main_route,
                 :main_route_no,
                 :description,
@@ -12,9 +12,18 @@ module Pids
                 :has_low_floor,
                 :last_modified
     
-    def initialize( route_no, headboard_route_no )
+    def initialize( route_no, headboard_route_no, internal_route_no, is_main_route, main_route_no,
+                    description, up_destination, down_destination, has_low_floor, last_modified )
       @route_no = route_no
       @headboard_route_no = headboard_route_no
+      @internal_route_no = internal_route_no
+      @is_main_route = is_main_route
+      @main_route_no = main_route_no
+      @description = description
+      @up_destination =  up_destination
+      @down_destination = down_destination
+      @has_low_floor = has_low_floor
+      @last_modified = last_modified
     end
 
   end
