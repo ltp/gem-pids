145792972f64eadd0b3e0743c1ae84b8dca61fc7
Luke Poskitt <luke.poskitt@gmail.com>
2015-12-11 16:20:25 +1100

    Added class Pids::PredictedArrivalTimeData.

---
+++ lib/pids/predicted_arrival_time_data.rb
@@ -0,0 +1,17 @@
+module Pids
+
+  class PredictedArrivalTimeData
+    attr_reader :tram_details, :predicted_stop_details
+
+    def initialize( data )
+      @tram_details = data[:tram_no_run_details_table]
+
+      data[:next_predicted_stops_details_table].each do |stop_detail|
+        @predicted_stop_details.push( Pids::PredictedStopDetail.new(stop_detail) )
+      end
+      
+    end
+
+  end
+
+end
