07f1c53f3ee86d9ad77c2058fb3d26e9afdf407d
Luke Poskitt <luke.poskitt@gmail.com>
2017-02-20 07:49:37 +1100

    Updated documentation for Pids::Destination

--- lib/pids/destination.rb
+++ lib/pids/destination.rb
@@ -2,6 +2,9 @@ module Pids
 
   class Destination
     # Create a new pids destination
+    # Note that under normal circumstances, it should not be necessary to 
+    # create a destination object directly - rather they will be cerated for 
+    # you automatically via calls to other methods.
     # @param route_no [Int] the route number of the destination.
     # @param up_stop [Boolean] indicates if the route destination is an up stop.
     # @param destination [String] description of the route destination as a 
