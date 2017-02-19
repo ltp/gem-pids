27cde8b57400c7cae8d2c09a2114ceb97146b120
Luke Poskitt <luke.poskitt@gmail.com>
2017-02-20 07:54:49 +1100

    Documentation.

--- lib/pids/listed_stop.rb
+++ lib/pids/listed_stop.rb
@@ -1,6 +1,15 @@
 module Pids
   
   class ListedStop
+  # Create a new Pids::ListedStop object.
+  # @param tid [Int] the trackerID of the stop.
+  # @param name [String] the name of the stop as a human-readable location.
+  # @param description [String] a description of the stop as a human-readable
+  #   description.
+  # @param latitude [Float] the latitude of the stop.
+  # @param longitude [Float] the longitude of the stop.
+  # @param suburb_name [String] the suburb in which the stop is located as a
+  #  human-readable value.
     attr_reader :tid,
                 :name,
                 :description,
