b31a3b583e0aa65fee3799d2f124f6f3605973e1
Luke Poskitt <luke.poskitt@gmail.com>
2017-02-20 07:48:05 +1100

    Added documentation for Pids::Destination

--- lib/pids/destination.rb
+++ lib/pids/destination.rb
@@ -1,6 +1,11 @@
 module Pids
 
   class Destination
+    # Create a new pids destination
+    # @param route_no [Int] the route number of the destination.
+    # @param up_stop [Boolean] indicates if the route destination is an up stop.
+    # @param destination [String] description of the route destination as a 
+    #  human-readable location.
     attr_reader :route_no, :up_stop, :destination
     
     def initialize( route_no, up_stop, destination )
